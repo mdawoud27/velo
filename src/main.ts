6717397e8ae6d7a6b51704c7466271571e2ee585
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2026-06-16 23:01:15 +0300

    ci: add generate prisma schema in github workflow

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,5 @@ async function bootstrap() {
   app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 import { Pool } from 'pg';
@@ -6,17 +6,18 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(PrismaService.name);
   constructor() {
-    const connectionString = `${process.env.DATABASE_URL}`;
+    const connectionString = process.env.DATABASE_URL ?? '';
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
     super({ adapter });
   }
-  async onModuleDestroy() {
+  async onModuleDestroy(): Promise<void> {
     await this.$disconnect();
   }
 
-  async onModuleInit() {
+  async onModuleInit(): Promise<void> {
     await this.$connect();
   }
 }
