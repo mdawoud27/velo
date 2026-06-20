57ed93a587189d799edcb95d494406702dd7c40a
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2026-06-20 19:33:13 +0300

    feat: add jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { RedisService } from 'src/redis/redis.service';
+import { JwtPayload } from '../interfaces';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly redis: RedisService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.getOrThrow<string>('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<JwtPayload> {
+    const isBlacklisted = await this.redis.exists(`blacklist:${payload.jti}`);
+    if (isBlacklisted) {
+      throw new UnauthorizedException('Token has been revoked');
+    }
+
+    return payload;
+  }
+}
