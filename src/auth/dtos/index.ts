8b0b0918fb780cc66e3e6df515578cbef2273fb7
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-06-23 22:52:32 +0300

    feat: add forgot and reset password endpoints - [AUTH-05] (#51)
    * feat: add forget password endpoint
    
    * feat: add `getdel` operation in redis service
    
    * feat: add reset password endpoint
    
    * fix: typo
    
    * refactor: auth service
    
    * refactor: auth and redis modules

--- src/auth/dtos/index.ts
+++ src/auth/dtos/index.ts
@@ -3,3 +3,5 @@ export * from './verify-email.dto';
 export * from './login.dto';
 export * from './resend-email.dto';
 export * from './refresh-token.dto';
+export * from './forgot-password.dto';
+export * from './reset-password.dto';
