eb18b8fb7220d8a2622ba680a04bf9e673e003c4
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-06-25 02:49:37 +0300

    feat: add v0.2.20

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,34 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import session from 'express-session';
+import { requestIdMiddleware } from './common/middlewares';
+import { RedisService } from './redis/redis.service';
+import { RedisSessionStore } from './common/session/redis-session.store';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  const redisService = app.get(RedisService);
+  const store = new RedisSessionStore(redisService);
+
+  app.use(requestIdMiddleware);
+  app.use(
+    session({
+      store,
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 10 * 60 * 1000,
+      },
+    }),
+  );
+
   app.useLogger(app.get(Logger));
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,6 +39,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
