3c92c94c9e0fdf8309067dce2104b5a1f83767a0
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-07-05 14:42:54 +0300

    refactor: pagination dto

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,14 +1,17 @@
 import { Type } from 'class-transformer';
-import { IsOptional, IsPositive } from 'class-validator';
+import { IsInt, IsOptional, IsPositive, Max } from 'class-validator';
 
 export class PaginationDto {
   @IsOptional()
   @Type(() => Number)
+  @IsInt()
   @IsPositive()
   page = 1;
 
   @IsOptional()
   @Type(() => Number)
+  @IsInt()
   @IsPositive()
+  @Max(100)
   limit = 10;
 }
