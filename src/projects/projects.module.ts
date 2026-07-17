bd19c639bb3eebd61503412f4863bf3e44d5daf3
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-07-17 11:50:27 +0300

    feat: enable notifications and realtime into projects module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
+import { NotificationsModule } from 'src/notifications/notifications.module';
+import { RealtimeModule } from 'src/realtime/realtime.module';
 
 @Module({
+  imports: [RealtimeModule, NotificationsModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
