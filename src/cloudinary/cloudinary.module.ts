65e83584edbf35a9ede2ceddce0691c9423b28e7
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-07-20 18:11:52 +0300

    chore: config multer

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,7 +1,32 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Module({
+  imports: [
+    MulterModule.registerAsync({
+      useFactory: () => ({
+        storage: memoryStorage(),
+        limits: { fieldSize: 10 * 1024 * 1024 },
+        fileFilter: (req, file, cb) => {
+          const allowed = [
+            'image/jpeg',
+            'image/png',
+            'image/gif',
+            'application/pdf',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          ];
+
+          if (allowed.includes(file.mimetype)) {
+            cb(null, true);
+          } else {
+            cb(new BadRequestException(`File type ${file.mimetype} not allowed`), false);
+          }
+        },
+      }),
+    }),
+  ],
   providers: [CloudinaryService],
   exports: [CloudinaryService],
 })
